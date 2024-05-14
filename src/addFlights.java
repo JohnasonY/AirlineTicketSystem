6f34b769147206c75a57c89a6a60a40a0ddf738e
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-05-14 16:45:58 -0400

    Update addFlights.java

--- src/addFlights.java
+++ src/addFlights.java
@@ -1,6 +1,14 @@
 import java.util.List;
 
-
-interface addFlights {
+/**
+ * An interface that can add a new flight
+ * @author Jiaxing Rong
+ */
+public interface addFlights {
+    /**
+     *
+     * @param flight a new flight to be added
+     * @param flights the flight list storing all the current available flights
+     */
     void addFlight(Flight flight, List<Flight> flights);
 }
